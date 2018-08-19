da1879bdfcaf356ba09a50ee8794397909c5e716
jessicadoucette <jessicadoucette15@gmail.com>
2018-08-19 14:44:58 -0600

    second commit, html, some css, some javascript built out

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+//list variables
+var goalCount = (Math.floor(Math.random())//insert math; 
+var currentTotal=[];
+var winCount = 0;
+var lossCount = 0;
+var gem1 = [];
+var gem2 = [];
+var gem3 = [];
+var gem4 = [];
+
+//get elements
+
+var $goalCount = document.getElementById('goal-count'
+);
+var $currentTotal = document.getElementById('current-total'); 
+var $winCount = document.getElementById('win-count');
+var $lossCount = document.getElementById('loss-count');
+
+//pick random number
+
+
+//have gems equal random numbers
+
+
+
+//crystal click function
+
+
+
+}
